658fd33eb48ba7f27f27c05042033a38db30f375
doom <61824552+JustDoom@users.noreply.github.com>
2025-07-15 17:56:06 +1000

    Switch to gradle

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+repositories {
+    mavenLocal()
+    maven {
+        url = uri("https://jitpack.io")
+    }
+
+    maven {
+        url = uri("https://repo.maven.apache.org/maven2/")
+    }
+}
+
+dependencies {
+    api(libs.com.github.lowercasebtw.minestom.v1.v21.v1)
+}
+
+group = "com.imjustdoom"
+version = "1.0-SNAPSHOT"
+description = "MinestomTest"
+java.sourceCompatibility = JavaVersion.VERSION_21
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+    }
+}
+
+tasks.withType<JavaCompile>() {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<Javadoc>() {
+    options.encoding = "UTF-8"
+}
